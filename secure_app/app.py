beb99b8e7444423d51fc5a0d061f487fd15f4717
abhiii265 <pattiriabhi@gmail.com>
2026-04-02 15:00:03 +0530

    Final Demo: Integrated Security Alerts and CSS Dashboard

--- secure_app/app.py
+++ secure_app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # FACULTY DEMO: The Vulnerability
 # To show a "Clean" site, comment out the line below.
 # Bandit will flag this as B105 (Hardcoded password) with HIGH severity
-ADMIN_PASSWORD = "AKIA5S6D7F8G9H0J1K2L_SECRET_KEY_EXPOSED_123456789"
+#ADMIN_PASSWORD = "AKIA5S6D7F8G9H0J1K2L_SECRET_KEY_EXPOSED_123456789"
 
 @app.route('/')
 def home():
